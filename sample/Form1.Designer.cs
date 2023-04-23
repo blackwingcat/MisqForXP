00faabcf26abf2193b00b75b2a202f33da8c224e
BlackWingCat <blackwingcat2000@gmail.com>
2023-04-23 12:21:00 +0900

    Add files via upload

---
+++ sample/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace mofkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tokenName = new System.Windows.Forms.TextBox();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.instanceName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tokenName
+            // 
+            this.tokenName.Location = new System.Drawing.Point(120, 33);
+            this.tokenName.Name = "tokenName";
+            this.tokenName.Size = new System.Drawing.Size(271, 19);
+            this.tokenName.TabIndex = 0;
+            this.tokenName.Text = "rivuDG6ohlkQYUQRslesfGAMhJj51MWq";
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(120, 60);
+            this.messageText.Multiline = true;
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(271, 77);
+            this.messageText.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "送信";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Token";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Instance";
+            // 
+            // instanceName
+            // 
+            this.instanceName.Location = new System.Drawing.Point(120, 8);
+            this.instanceName.Name = "instanceName";
+            this.instanceName.Size = new System.Drawing.Size(271, 19);
+            this.instanceName.TabIndex = 5;
+            this.instanceName.Text = "https://misskey.io";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 194);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.instanceName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.tokenName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tokenName;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox instanceName;
+    }
+}
+
